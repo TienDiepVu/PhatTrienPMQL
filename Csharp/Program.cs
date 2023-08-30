07b234da04c7a2e6ee51aeeb69f8b74e7ec0a935
TienDiepVu <tiendiepvu2001@gmail.com>
2023-08-30 23:51:20 +0700

    add bai tap

---
+++ Csharp/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a, b;
+        Console.Write("Nhập vào số thứ nhất: a= ");
+        a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Nhập vào số thứ hai: b= ");
+        b = Convert.ToInt32(Console.ReadLine());
+        if (a == b) 
+        {
+            Console.Write(a + " bằng " + b);
+        }
+        else 
+        {
+            if (a > b){ 
+            Console.WriteLine(a + " lớn hơn " + b);
+            }
+            else {
+            Console.WriteLine(a + " nhỏ hơn " + b);
+            }
+        }       
+    }
+}
