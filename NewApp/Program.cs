a49b69ab617da54189a72737c85c7ccea0ad77a3
TienDiepVu <tiendiepvu2001@gmail.com>
2023-09-07 18:05:48 +0700

    thuc hanh toan tu so hoc

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,11 +1,20 @@
 ﻿//Khai bao bien
 string name = "Vu Tien Diep";
 int age = 22;
-Console.WriteLine("Sinh Vien : {0} - {1} tuoi ", name , age);
+Console.WriteLine("Sinh Vien : {0} - {1} tuoi ", name, age);
 
 //khai bao hang 
 const string hoTen = "Vu Tien Diep";
 const int tuoi = 22;
-Console.WriteLine( "Sinh Vien : {0} - {1} tuoi ", hoTen, tuoi);
+Console.WriteLine("Sinh Vien : {0} - {1} tuoi ", hoTen, tuoi);
 
-
+// Toan tu so hoc
+int a, b;
+Console.Write("Nhap vao so thu nhat: a = ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Nhap vao so thu hai: b = ");
+b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("{0} - {1} = {2} ", a, b, a - b);
+Console.WriteLine("{0} * {1} = {2} ", a, b, a * b);
+Console.WriteLine("{0} / {1} = {2} ", a, b, a / b);
+Console.WriteLine("{0} % {1} = {2} ", a, b, a % b);
