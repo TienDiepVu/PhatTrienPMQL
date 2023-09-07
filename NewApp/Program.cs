a7c02ca108a3140ff72f69211f8e80d8cb66da14
TienDiepVu <tiendiepvu2001@gmail.com>
2023-09-07 18:00:56 +0700

    khai bao hang

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -3,4 +3,9 @@ string name = "Vu Tien Diep";
 int age = 22;
 Console.WriteLine("Sinh Vien : {0} - {1} tuoi ", name , age);
 
+//khai bao hang 
+const string hoTen = "Vu Tien Diep";
+const int tuoi = 22;
+Console.WriteLine( "Sinh Vien : {0} - {1} tuoi ", hoTen, tuoi);
+
 
