6449d12238c7e772d2ee4935624701f88ef78c74
TienDiepVu <tiendiepvu2001@gmail.com>
2023-09-29 01:24:40 +0700

    declare variable

---
+++ Cs-basic/Program.cs
@@ -0,0 +1,9 @@
+﻿public  class Program 
+{
+    public static void Main(string[] args)
+    {
+        string name = "Vu Tien Diep";
+        int age = 22;
+        System.Console.WriteLine("Thông tin sinh viên: {0} - {1}", name, age);
+    }
+}
