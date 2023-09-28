b8dfa04d54c546a39ec9fdb108a6aca3233e9f37
TienDiepVu <tiendiepvu2001@gmail.com>
2023-09-29 02:41:11 +0700

    add class Person

---
+++ NewApp/Models/Person.cs
@@ -0,0 +1,19 @@
+namespace NewApp.Models
+{
+    public class Person
+    {
+        public string FullName{ get; set; }
+        public int Age { get; set;}
+        public void Import()
+        {
+            Console.Write("Ho va Ten: ");
+            FullName = Console.ReadLine();
+            Console.Write("Tuoi: ");
+            Age = Convert.ToInt16(Console.ReadLine());
+        }
+        public void Export()
+        {
+            Console.WriteLine("{0} - {1}", FullName , Age);
+        }
+    }
+}
