cdd1d6e2def4121df37c09a5692298d46986417e
TienDiepVu <tiendiepvu2001@gmail.com>
2023-09-29 01:46:50 +0700

    declare const

--- Cs-basic/Program.cs
+++ Cs-basic/Program.cs
@@ -2,8 +2,16 @@
 {
     public static void Main(string[] args)
     {
+        /* Khai bao bien
         string name = "Vu Tien Diep";
         int age = 22;
-        System.Console.WriteLine("Thông tin sinh viên: {0} - {1}", name, age);
+        System.Console.WriteLine("Thong tin sinh vien: {0} - {1}", name, age);
+        */
+        // Khai bao hang 
+        const string employee = "Vu Tien Diep";
+        const int born = 2001;
+        const string company = " Cong ty Co phan Cong nghe va Truyen thon RT";
+        const float exp = 0.5f;
+        System.Console.WriteLine("Ho va ten :{0} \n Nam sinh: {1} \n Don vi cong tac: {2}\n Tham nien: {3} nam ", employee , born , company , exp  );
     }
 }
