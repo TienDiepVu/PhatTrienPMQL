c6744b906a43b9323736996ff7cbe5cd405c1db0
TienDiepVu <tiendiepvu2001@gmail.com>
2023-10-01 17:18:45 +0700

    phuong thuc khoi tao

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -3,11 +3,11 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        SinhVien st = new SinhVien();
-        st.HoTen = "Vu Tien Diep" ;
-        st.DiaChi= "Ha Noi";
-        st.Tuoi = 22;
-        st.SoDienThoai = "0984523361";
-        st.HienThiThongTinSV();
+        Person ps1 = new Person();
+        Person ps2 = new Person();
+        ps1.FullName = "Vu Tien Diep";
+        ps1.Age = 22;
+        ps1.Export();
+        ps2.Export();
     }
 }
