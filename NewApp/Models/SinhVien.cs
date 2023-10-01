1090cdf96469510494da86676cf1a4a32d5adeca
TienDiepVu <tiendiepvu2001@gmail.com>
2023-10-01 17:08:33 +0700

    add class sinhvien

---
+++ NewApp/Models/SinhVien.cs
@@ -0,0 +1,29 @@
+namespace NewApp.Models
+{
+    public class SinhVien
+    {
+        public string HoTen { get; set;}
+        public string DiaChi { get; set;}
+        public int Tuoi { get; set;}
+        public string SoDienThoai { get; set;}
+
+        public void NhapThongTinSV()
+        {
+            Console.Write("Ho va ten: ");
+            HoTen = Console.ReadLine();
+            Console.Write("Dia chi: ");
+            DiaChi = Console.ReadLine();
+            Console.Write("Tuoi: ");
+            Tuoi = Convert.ToInt16(Console.ReadLine());
+            Console.Write("So dien thoai: ");
+            SoDienThoai = Console.ReadLine();
+            
+        }
+
+        public void HienThiThongTinSV() 
+        {
+            Console.WriteLine("Ho va ten: " + HoTen + "\n Dia chi: " + DiaChi + "\n Tuoi: " + Tuoi + "\n So dien thoai: " + SoDienThoai );
+        }
+        
+    }
+}
--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -3,11 +3,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Employee fr = new Employee();
-        fr.NameOfEmp = "Cam";
-        fr.AgeOfEmp = 22;
-        fr.EmpID = 110701;
-        fr.Salary = 25000000;
-        fr.ExportData();
+        
+
     }
 }
