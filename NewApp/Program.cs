0e0a77e5b6e508f43b3939b50ae942c85e2ba420
TienDiepVu <tiendiepvu2001@gmail.com>
2023-10-01 17:10:41 +0700

    tao doi tuong voi class sinh vien

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -3,7 +3,11 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        
-
+        SinhVien st = new SinhVien();
+        st.HoTen = "Vu Tien Diep" ;
+        st.DiaChi= "Ha Noi";
+        st.Tuoi = 22;
+        st.SoDienThoai = "0984523361";
+        st.HienThiThongTinSV();
     }
 }
