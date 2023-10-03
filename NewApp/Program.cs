5993a2afc0e77c2c7acc72be21a050b0ccd0026d
TienDiepVu <tiendiepvu2001@gmail.com>
2023-10-04 01:50:38 +0700

    gan gia tri vao mang

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,6 +1,10 @@
 ﻿using NewApp.Models;
 int[] intArr = new int[5]{1,2,3,4,5};
+intArr[5] = 10;
+
 Student[] stdArr = new  Student[3];
-Employee[] empArr;
-empArr = new Employee[2];
+Student std = new Student ();
+std.Import();
+stdArr[3] = std;
+
 
