d52cb39d270994f14101595279c281fb16729f65
TienDiepVu <tiendiepvu2001@gmail.com>
2023-10-04 23:49:08 +0700

    array list

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,7 +1,17 @@
 ﻿using NewApp.Models;
 using System.Collections;
 ArrayList arrList = new ArrayList();
-
+for(int i = 0; i < 2; i++)
+{
+Student std = new Student (); 
+std.Import(); 
+arrList.Add(std); 
+}
+for(int i = 0; i < 2; i++)
+{
+Student std = (Student) arrList[i];
+std.Export(); 
+}
 
 
 
