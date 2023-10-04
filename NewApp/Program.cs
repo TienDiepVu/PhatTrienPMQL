c5894d369e2a239cdb50fc7337fac36728b9d7e1
TienDiepVu <tiendiepvu2001@gmail.com>
2023-10-04 23:27:28 +0700

    su dung arr nhap va hien thi du lieu

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,17 +1,19 @@
 ﻿using NewApp.Models;
-Person[] stdArr = new  Person[3];
+Employee[] stdArr = new  Employee[3];
 for(int i = 0; i < 3; i++)
 {
 Console.WriteLine("Nhap phan tu thu {0}: ", i);
-Person std = new Person ();
+Employee std = new Employee ();
 std.Import();
 stdArr[i] = std;
 }
-foreach(Person std in stdArr)
+foreach(Employee std in stdArr)
 {
 string fName = std.FullName;
 string PID = std.PersonID;
-Console.WriteLine("{0} – {1}", fName, PID);
+int a = std.Age;
+int slr = std.Salary;
+Console.WriteLine("{0} – {1} - {3} - {2} USD", fName, PID, slr , a);
 }
 
 
