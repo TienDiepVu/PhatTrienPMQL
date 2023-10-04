280d18493f0c3b9c02faa2891bedcacd80e857e2
TienDiepVu <tiendiepvu2001@gmail.com>
2023-10-04 23:48:33 +0700

    collection

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,20 +1,9 @@
 ﻿using NewApp.Models;
-Employee[] stdArr = new  Employee[3];
-for(int i = 0; i < 3; i++)
-{
-Console.WriteLine("Nhap phan tu thu {0}: ", i);
-Employee std = new Employee ();
-std.Import();
-stdArr[i] = std;
-}
-foreach(Employee std in stdArr)
-{
-string fName = std.FullName;
-string PID = std.PersonID;
-int a = std.Age;
-int slr = std.Salary;
-Console.WriteLine("{0} – {1} - {3} - {2} USD", fName, PID, slr , a);
-}
+using System.Collections;
+ArrayList arrList = new ArrayList();
+
+
+
 
 
 
