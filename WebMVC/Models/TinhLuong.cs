66940b2868591156547423932c7600d8dabefcfb
TienDiepVu <tiendiepvu2001@gmail.com>
2023-10-25 10:07:53 +0700

    update Tinh Luong nhan vien

---
+++ WebMVC/Models/TinhLuong.cs
@@ -0,0 +1,8 @@
+namespace WebMVC.Models;
+public class TinhLuong
+{
+    public double LuongCoBan { get; set; }
+    public double HeSoLuong { get; set; }
+    public double PhuCap { get; set; }
+    public double TongLuong { get; set; }
+}
