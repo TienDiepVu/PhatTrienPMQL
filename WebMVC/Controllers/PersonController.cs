736b70deeb5c009eb63431e7e8822bbe110532bd
TienDiepVu <tiendiepvu2001@gmail.com>
2023-12-08 09:32:24 +0700

    Excel.

--- WebMVC/Controllers/PersonController.cs
+++ WebMVC/Controllers/PersonController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebMVC.Models;
 using WebMVC.Data;
+using WebMVC.Models.Process;
 
 namespace WebMVC.Controllers
 {
@@ -12,6 +13,7 @@ namespace WebMVC.Controllers
         {
             _context = context;
         }
+        private ExcelProcess _excelPro = new ExcelProcess();
         public async Task<IActionResult> Index()
         {
             var model = await _context.Person.ToListAsync();
@@ -112,7 +114,52 @@ namespace WebMVC.Controllers
         }
         private bool PersonExists(string id)
         {
-            return (_context.Person?.Any(e =>e.PersonID == id)).GetValueOrDefault();
+            return (_context.Person?.Any(e => e.PersonID == id)).GetValueOrDefault();
+        }
+        public IActionResult Upload()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            if (file != null)
+            {
+                string fileExtension = Path.GetExtension(file.FileName);
+                if (fileExtension != ".xls" && fileExtension != ".xlsx")
+                {
+                    ModelState.AddModelError("", "Please choose excel file to upload!");
+                }
+                else
+                {
+                    //rename file when upload to server
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory() + "/Uploads/Excels", "File" + DateTime.Now.Day + DateTime.Now.Hour + DateTime.Now.Minute + DateTime.Now.Millisecond + fileExtension);
+                    var fileLocation = new FileInfo(filePath).ToString();
+                    if (file.Length > 0)
+                    {
+                        using (var stream = new FileStream(filePath, FileMode.Create))
+                        {
+                            //save file to server
+                            await file.CopyToAsync(stream);
+                            //read data from file and write to database
+                            var dt = _excelPro.ExcelToDataTable(fileLocation);
+                            for (int i = 0; i < dt.Rows.Count; i++)
+                            {
+                                var ps = new Person();
+                                ps.PersonID = dt.Rows[i][0].ToString();
+                                ps.FullName = dt.Rows[i][1].ToString();
+                                ps.Address = dt.Rows[i][2].ToString();
+                                ps.Phone = dt.Rows[i][3].ToString();
+                                _context.Add(ps);
+                            }
+                            await _context.SaveChangesAsync();
+                            return RedirectToAction(nameof(Index));
+                        }
+                    }
+                }
+            }
+
+            return View();
         }
 
     }
--- WebMVC/Models/DaiLy.cs
+++ WebMVC/Models/DaiLy.cs
@@ -1,6 +1,5 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore.Sqlite;
 
 namespace WebMVC.Models
 {
